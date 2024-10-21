a090fbf27c526a6937a40dc2c31b3ab947b3b570
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-21 19:32:15 +0530

    Covered basic data types

---
+++ 1.Syntax & data types/data_types.py
@@ -0,0 +1,33 @@
+# This is a single line comment.
+
+'''
+This is multi line comment, these are single quotes, not backticks.
+'''
+
+# Integer
+a = 10
+print("a is an integer: {a}")
+
+# Float
+b = 20.5
+print("b is a float: {b}")
+
+# String
+c = "Hello, Python!"
+print("c is a string: {c}")
+
+# Boolean
+d = True
+print("d is a boolean: {d}")
+
+# List
+e = [1, 2, 3, 4, 5]
+print("e is a list: {e}")
+
+# Dictionary
+f = {"name": "Lohit", "age": 30}
+print("f is a dictionary: {f}")
+
+# Tuple
+g = (1,2,3)
+print("g is a tuple: {g}")
---
+++ 1.Syntax & data types/data_types_f.py
@@ -0,0 +1,33 @@
+# This is a single line comment.
+
+'''
+This is multi line comment, these are single quotes, not backticks.
+'''
+
+# Integer
+a = 10
+print(f"a is an integer: {a}")
+
+# Float
+b = 20.5
+print(f"b is a float: {b}")
+
+# String
+c = "Hello, Python!"
+print(f"c is a string: {c}")
+
+# Boolean
+d = True
+print(f"d is a boolean: {d}")
+
+# List
+e = [1, 2, 3, 4, 5]
+print(f"e is a list: {e}")
+
+# Dictionary
+f = {"name": "Lohit", "age": 30}
+print(f"f is a dictionary: {f}")
+
+# Tuple
+g = (1,2,3)
+print(f"g is a tuple: {g}")
