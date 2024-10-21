24f1418eb33767db93ef8c553f8481ea849bd073
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-21 20:31:29 +0530

    Covered basic function creation and return argument

---
+++ 3.Functions/basic-func-def.py
@@ -0,0 +1,6 @@
+# Defining a function
+def add_numbers(a,b):
+  return a + b
+
+result = add_numbers(5,5)
+print(result)
---
+++ 3.Functions/basic-func-task-return.py
@@ -0,0 +1,4 @@
+def greet(name, age):
+  return(f"Hello!, {name}! You are {age} years old.")
+
+print(greet("Lohit", 30))
